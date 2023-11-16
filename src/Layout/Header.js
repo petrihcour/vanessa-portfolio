b021f94c371d5701411d4d92f2af94e3ad6bba0b
Vanessa <zdperfectx@gmail.com>
2023-11-16 11:34:57 -0600

    got columns correct for header w/ bootstrap. working on aligning to left and right

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -6,18 +6,20 @@ function Header() {
   return (
     <nav className="navbar bg-body-tertiary fixed-top">
       <div className="container-fluid header">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-8">
-              <h3>Vanessa Garcia</h3>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-3">
+              <h3 className="">Vanessa Garcia</h3>
+            </div>
+            <div className="col-3">
               <h3>Frontend Developer</h3>
             </div>
-            <div class="col-4 text-end">
+            <div className="col-6 text-end">
               <Link to="/" className="navbar-brand">
                 <img
                   src="https://cdn.iconfinder.com/icons/4683694/5155940/1024/raster.png?token=1700153899-BGXowyJ2GNVAKFc4EM1mKwubdLIMi9Hytpw4WBJevfI%3D"
                   width="35"
-                  alt="20 sided die witg crossing swords"
+                  alt="20 sided die with crossing swords"
                 />
               </Link>
             </div>
