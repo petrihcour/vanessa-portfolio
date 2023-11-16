c3427ac57426176f65d1b58ead4f26a8ce901774
Vanessa <zdperfectx@gmail.com>
2023-11-16 11:57:34 -0600

    successfully aligned how i want. need to work on paading and vertical alignment

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,18 +4,16 @@ import "../styles/Header.css";
 
 function Header() {
   return (
-    <nav className="navbar bg-body-tertiary fixed-top">
-      <div className="container-fluid header">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-3">
-              <h3 className="">Vanessa Garcia</h3>
+    <nav className="fixed-top text-bg-dark">
+          <div className="d-flex align-items-center">
+            <div className="p-2">
+              <h3>Vanessa Garcia</h3>
             </div>
-            <div className="col-3">
+            <div className="p-2">
               <h3>Frontend Developer</h3>
             </div>
-            <div className="col-6 text-end">
-              <Link to="/" className="navbar-brand">
+            <div className="ms-auto p-2">
+              <Link to="/">
                 <img
                   src="https://cdn.iconfinder.com/icons/4683694/5155940/1024/raster.png?token=1700153899-BGXowyJ2GNVAKFc4EM1mKwubdLIMi9Hytpw4WBJevfI%3D"
                   width="35"
@@ -23,8 +21,7 @@ function Header() {
                 />
               </Link>
             </div>
-          </div>
-        </div>
+          
       </div>
     </nav>
   );
