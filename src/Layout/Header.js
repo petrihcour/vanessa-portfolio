b3a595cc84edc5d890b75938ac42f69050c10d80
Vanessa <zdperfectx@gmail.com>
2023-11-16 11:11:59 -0600

    working thorugh alignment of header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -2,20 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
-
-
 function Header() {
   return (
     <nav className="navbar bg-body-tertiary fixed-top">
       <div className="container-fluid header">
-        <Link to="/" className="navbar-brand" >
-          <h3>
-            Vanessa Garcia
-          </h3>
-          <h3>Frontend Developer</h3>
-          <img src="https://cdn.iconfinder.com/icons/4683694/5155940/1024/raster.png?token=1700153899-BGXowyJ2GNVAKFc4EM1mKwubdLIMi9Hytpw4WBJevfI%3D" width="35" alt="20 sided die witg crossing swords"/>
-        </Link>
-       
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-8">
+              <h3>Vanessa Garcia</h3>
+              <h3>Frontend Developer</h3>
+            </div>
+            <div class="col-4 text-end">
+              <Link to="/" className="navbar-brand">
+                <img
+                  src="https://cdn.iconfinder.com/icons/4683694/5155940/1024/raster.png?token=1700153899-BGXowyJ2GNVAKFc4EM1mKwubdLIMi9Hytpw4WBJevfI%3D"
+                  width="35"
+                  alt="20 sided die witg crossing swords"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
