7458145230a506dfa15d2bd88a3a3db7eb278ed6
Vanessa <zdperfectx@gmail.com>
2023-11-20 10:27:49 -0600

    adjusted sizing in home to be paragraph and added draft intro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,10 +10,11 @@ function Home() {
             <img src={vanessa} alt="AI representation of Vanessa" className="w-50 rounded-circle float-start border border-secondary-subtle p-2" />
             <div className="">
             <h1>Character Info:</h1>
-            <h3>Name: Vanessa</h3>
-            <h3>Class: Frontend Developer</h3>
-            <h3>Background: Digital Artisan</h3>
-            <h3>Alignment: Chaotic Creative</h3>
+            <p>Name: Vanessa</p>
+            <p>Class: Frontend Developer</p>
+            <p>Background: Digital Artisan</p>
+            <p>Alignment: Chaotic Creative</p>
+            <p>Introduction: Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction Introduction  </p>
             </div>
         </div>
     )
