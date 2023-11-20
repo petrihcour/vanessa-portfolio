4e04ece41aed07db346d34b397ae8062b30aa467
Vanessa <zdperfectx@gmail.com>
2023-11-20 09:44:24 -0600

    image was no longer showing up. input link again. adjusted sizing and spacing for header using bootstrap spacing

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -6,16 +6,16 @@ function Header() {
   return (
     <nav className="fixed-top text-bg-dark">
           <div className="d-flex align-items-center">
-            <div className="p-2">
-              <h3>Vanessa Garcia</h3>
+            <div className="p-3 ">
+              <h5>Vanessa Garcia</h5>
             </div>
-            <div className="p-2">
-              <h3>Frontend Developer</h3>
+            <div className="p-3 d-none d-sm-block">
+              <h5>Frontend Developer</h5>
             </div>
-            <div className="ms-auto p-2">
+            <div className="ms-auto p-3">
               <Link to="/">
                 <img
-                  src="https://cdn.iconfinder.com/icons/4683694/5155940/1024/raster.png?token=1700153899-BGXowyJ2GNVAKFc4EM1mKwubdLIMi9Hytpw4WBJevfI%3D"
+                  src="https://cdn.iconfinder.com/icons/4683694/5155940/1024/raster.png?token=1700492748-5vrspDSNyFSanYC4BWN3HxA8XSXwsI1H7uYbemiaH7c%3D"
                   width="35"
                   alt="20 sided die with crossing swords"
                 />
