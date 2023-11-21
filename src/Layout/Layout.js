ce359c807d1111b1071f8cdc34977661fdc605c2
Vanessa <zdperfectx@gmail.com>
2023-11-21 10:08:54 -0600

    background moved to index.css to cover entire body, adjusted background color of nav and header. will adjust later

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -7,13 +7,11 @@ import Home from "../pages/Home";
 import Quests from "../pages/Quests";
 import Sending from "../pages/Sending";
 import Skills from "../pages/Skills";
-import background from "../images/background.jpg";
+
 
 function Layout() {
   return (
-    <div className="Layout" style={{backgroundImage: `url(${background})`,
-    backgroundSize: "cover", height: "100vh",
-    }}>
+    <div className="Layout" >
       <Header />
       <div className="mt-5 pt-5">
       <Switch>
--- src/Layout/MobileNav.js
+++ src/Layout/MobileNav.js
@@ -7,7 +7,7 @@ import "../styles/MobileNav.css";
 
 function MobileNav() {
   return (
-    <nav className="navbar fixed-bottom bg-body-tertiary d-md-none d-lg-block">
+    <nav className="navbar fixed-bottom bg-transparent text-body p-2 d-md-none d-lg-none">
       <div className="container-fluid">
           <Link to="/" className="navbar-brand ms-1">
             <div className="nav-item home-icon">
