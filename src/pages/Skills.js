f8b6cee497b752d3eb9df57adda882ea6266dde6
Vanessa <zdperfectx@gmail.com>
2023-11-23 16:20:50 -0600

    added responsive card size in style component. added list group flush to go intside of it

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 function Skills() {
-    return (
-        <h1>Skills</h1>
-    )
+  return (
+    <div className="card" style={{maxwidth: "540px"}}>
+  <div className="card-body">
+  <h2 className="card-title text-center">SKILLS</h2>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    {/* <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a> */}
+  </div>
+</div>
+      
+    
+  );
 }
 
 export default Skills;
