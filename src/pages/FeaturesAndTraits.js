a8320c347c56de86cff86c6519da9a978fbb3c8f
Vanessa <zdperfectx@gmail.com>
2023-11-26 17:41:06 -0600

    added working to featuresandtraits aboutme now working on styling

--- src/pages/FeaturesAndTraits.js
+++ src/pages/FeaturesAndTraits.js
@@ -1,11 +1,38 @@
 import React from "react";
 
 function FeaturesAndTraits() {
-    return (
-        <div className="p-3">
-            <h2 className="text-center">FEATURES & TRAITS</h2>
-        </div>
-    )
+  return (
+    <div className="p-3">
+      <h2 className="text-center p-3">FEATURES & TRAITS</h2>
+      <p>
+        Greetings, Adventurer! I'm Vanessa, a Frontend Developer on a quest for
+        digital excellence and collaborative magic. 🧙‍♀️✨
+      </p>
+      <p>
+        In the enchanted landscape of technology, I've mastered the arcane
+        languages of JavaScript, React, HTML, and CSS, crafting immersive
+        virtual experiences that transcend the ordinary. My journey isn't just
+        about writing code; it's a tale of leadership, problem-solving, and
+        blending the art of frontend development with the wisdom gained from
+        over five years in leadership roles.
+      </p>
+      <p>
+        In the realms beyond code, I'm Vanessa, a seeker of seamless
+        experiences, a collaborator in the forge of innovation, and a traveler
+        across the vast landscapes of the United States. My passion for
+        enhancing technology to improve lives is matched only by my love for
+        Dungeons & Dragons, live music, and the thrill of exploration.
+        <p>
+          Join Me on this Quest! If you're on a quest for a developer with
+          technical prowess, a strong collaboration spirit, and a penchant for
+          delivering impactful results, let's embark on this digital journey
+          together. Connect with me on LinkedIn or explore my coding quests on
+          GitHub. Together, we'll weave the magic of technology and create
+          experiences that resonate across the digital realms! 🌐🔮✨
+        </p>
+      </p>
+    </div>
+  );
 }
 
 export default FeaturesAndTraits;
