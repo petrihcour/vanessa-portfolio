46937f2e42fc0e2386380d273ad36b4e74044448
Vanessa <zdperfectx@gmail.com>
2023-12-11 10:03:44 -0600

    card shows current index of quest and matches info per card slife

--- src/quest-data/QuestData.js
+++ src/quest-data/QuestData.js
@@ -6,7 +6,13 @@ const QuestData = [
         description: "jhjhjkhjkhjkhjkhjkhjkhjkhhjhkjhj",
         imageUrl: flashcardProject,
         webUrl: "https://github.com/petrihcour/flashcard-app.git",
-    }
+    }, 
+    {
+        name: "jkjkjkj-o-matic", 
+        description: "888ujhjkhjjhjhjhjhjhjh",
+        imageUrl: flashcardProject,
+        webUrl: "https://github.com/petrihcour/flashcard-app.git",
+    },
 ]
 
 export default QuestData;
--- src/quest-data/QuestTemplate.js
+++ src/quest-data/QuestTemplate.js
@@ -1,70 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestData from "./QuestData";
 
 function QuestTemplate() {
-  // const quest =
-  //   <hr />
-  //   <div className="card-body">
-  //     <h5 className="card-title text-center">{quest.name}</h5>
-  //     <p className="card-text text-center">{quest.description}</p>
-  //     <a href={quest.webUrl} className="btn btn-dark active" role="button" aria-pressed="true" >Link</a>
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  //   </div>
-  //   </div>
-  // ));
+  const handleSlideChange = (index) => {
+    setActiveIndex(index);
+  };
 
   return (
     <div>
-    <div id="carouselExampleFade" className="carousel slide carousel-fade">
-      <div className="carousel-inner">
-        {QuestData.map((quest, index) => (
-          <div
-            key={index}
-            className={`carousel-item ${index === 0 ? "active" : ""}`}
+      <div id="carouselExampleFade" className="carousel slide carousel-fade">
+        <div className="carousel-inner">
+          {QuestData.map((quest, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${
+                index === activeIndex ? "active" : ""
+              }`}
+              onClick={() => handleSlideChange(index)}
+            >
+              <img
+                src={quest.imageUrl}
+                className="d-block w-100"
+                alt={quest.name}
+              />
+            </div>
+          ))}
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+            onClick={() =>
+              handleSlideChange(
+                (activeIndex - 1 + QuestData.length) % QuestData.length
+              )
+            }
           >
-            <img
-              src={quest.imageUrl}
-              className="d-block w-100"
-              alt={quest.name}
-            />
-          </div>
-        ))}
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+            onClick={() =>
+              handleSlideChange((activeIndex + 1) % QuestData.length)
+            }
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
-    </div>
-    <br />
-    <div className="card text-center">
-        {QuestData.map((quest, index) => (
-          <div key={index} className="card-body">
-            <h5 className="card-title">{quest.name}</h5>
-            <p className="card-text">{quest.description}</p>
-            <a href={quest.webUrl} className="btn btn-dark" target="_blank" rel="noreferrer">Button</a>
-             </div>
-        ))}
-    </div>
+      <br />
+      <div className="card text-center">
+        <div className="card-body">
+          <h5 className="card-title">{QuestData[activeIndex].name}</h5>
+          <p className="card-text">{QuestData[activeIndex].description}</p>
+          <a
+            href={QuestData[activeIndex].webUrl}
+            className="btn btn-dark"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Button
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
