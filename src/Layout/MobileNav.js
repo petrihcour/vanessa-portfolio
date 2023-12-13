7e6c8310001222c911bfb45ca7fa296d2fce2e8c
Vanessa <zdperfectx@gmail.com>
2023-12-13 10:26:46 -0600

    separated offcasnvas in mobilenav to its own component due to excess of code. calling it in mobile nav

--- src/Layout/MobileNav.js
+++ src/Layout/MobileNav.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import OffCanvas from "./OffCanvas";
 import "../styles/MobileNav.css";
+
 
 // add design a little bit later. possible gold trim. adjust font for slighlty more ancient feeling
 // small animations for buttons / links / menu
@@ -67,70 +69,7 @@ function MobileNav() {
               <div className="small-word menu-text">Menu</div>
          
           </button>
-        
-        <div
-          className="offcanvas offcanvas-end"
-          tabIndex="-1"
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-        >
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-            Unleash your spells. <br />
-             I cast...
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <Link to="/" className="nav-link" aria-current="page">
-                  Teleport
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/sending" className="nav-link">
-                  Sending
-                </Link>
-              </li>
-              <li className="nav-item dropdown">
-                <Link to=""
-                  className="nav-link dropdown-toggle"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Locate Creature
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a href="https://www.linkedin.com/in/vanesssagarcia/" target="_blank" 
-                    rel="noreferrer"
-                    className="dropdown-item">
-                      LinkedIn
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a href="https://github.com/petrihcour" 
-                    target="_blank" 
-                    rel="noreferrer"
-                    className="dropdown-item">
-                      GitHub
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
+          <OffCanvas />
       </div>
     </nav>
   );
