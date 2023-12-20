a3a43bc698df1150cd1096f506fb8f859c538503
Vanessa <zdperfectx@gmail.com>
2023-12-20 09:55:48 -0600

    created calculation for frontend backend and personal bonuses and added to expertise or proficiency bonus in skills data

--- src/skills-data/SkillsData.js
+++ src/skills-data/SkillsData.js
@@ -1,76 +1,80 @@
-
-
 // bonuses
 // might implement dice rolling for bonus
 const proficiencyBonus = 3;
 const expertiseBonus = proficiencyBonus + proficiencyBonus;
+
+const bonusData = {
+  Frontend: 4,
+  Backend: 3,
+  Personal: 5,
+};
 
 const skillsData = [
   {
     proficiency: "expertise",
     type: "Frontend",
     skill: "Bootstrap",
-    bonus: expertiseBonus,
+    bonus: expertiseBonus + bonusData.Frontend,
   },
   {
     proficiency: "proficient",
     type: "Personal",
     skill: "Cross-Functional Collaborations",
-    bonus: proficiencyBonus,
+    bonus: proficiencyBonus + bonusData.Personal,
   },
   {
     proficiency: "proficient",
     type: "Frontend",
     skill: "CSS",
-    bonus: proficiencyBonus,
+    bonus: proficiencyBonus + bonusData.Frontend,
   },
   {
     proficiency: "proficient",
     type: "Backend",
     skill: "Express Js",
-    bonus: proficiencyBonus,
+    bonus: proficiencyBonus + bonusData.Backend,
   },
   {
-    proficiency: "proficient",
+    proficiency: "expertise",
     type: "Frontend",
     skill: "HTML5",
-    bonus: proficiencyBonus,
+    bonus: expertiseBonus + bonusData.Frontend,
   },
   {
     proficiency: "proficient",
     type: "Frontend",
     skill: "JavaScript",
-    bonus: proficiencyBonus,
+    bonus: proficiencyBonus + bonusData.Frontend,
   },
   {
     proficiency: "proficient",
     type: "Backend",
     skill: "Node Js",
-    bonus: proficiencyBonus,
+    bonus: proficiencyBonus + bonusData.Backend,
   },
   {
     proficiency: "proficient",
     type: "Backend",
     skill: "PostgreSQL",
-    bonus: proficiencyBonus,
+    bonus: proficiencyBonus + bonusData.Backend,
   },
   {
-    proficiency: "expertise",
+    proficiency: "proficient",
     type: "Personal",
     skill: "Project Management",
-    bonus: expertiseBonus,
+    bonus: proficiencyBonus + bonusData.Personal,
   },
   {
     proficiency: "proficient",
     type: "Frontend",
     skill: "React",
-    bonus: proficiencyBonus,
+    bonus: proficiencyBonus + bonusData.Frontend,
   },
   {
     proficiency: "expertise",
     type: "Personal",
     skill: "Root Cause Problem Solving",
-    bonus: expertiseBonus,
+    bonus: expertiseBonus + bonusData.Personal,
   },
 ];
 
