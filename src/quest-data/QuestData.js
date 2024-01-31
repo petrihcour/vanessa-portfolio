3b13c3826617d52e8152b2c4089adac4c60b2ac3
Vanessa <zdperfectx@gmail.com>
2024-01-31 10:26:51 -0600

    updated quest images, hithub url, and weburl

--- src/quest-data/QuestData.js
+++ src/quest-data/QuestData.js
@@ -1,29 +1,36 @@
-import flashcardProject from "../images/quests/flashcardProject.jpeg";
+import flashcard from "../images/quests/flashcard.jpeg";
+import thinkfulbnb from "../images/quests/thinkfulbnb.jpeg";
+import welovemovies from "../images/quests/welovemovies.jpeg";
+import restaurantReservation from "../images/quests/restaurant-reservation.png";
 
 const QuestData = [
     {
         name: "FlashCard-o-matic", 
         description: "An online study application that gives teachers the ability to create, read, update, edit, and delete decks and cards for their students to study.",
-        imageUrl: flashcardProject,
-        webUrl: "https://github.com/petrihcour/flashcard-app.git",
+        imageUrl: flashcard,
+        gitHubUrl: "https://github.com/petrihcour/flashcard-app.git",
+        webUrl: "",
     }, 
     {
         name: "ThinkfulBnB", 
         description: "A static website that emulates AirBnb and focuses on a responsive, intuitive user interface with a mobile-first approach.",
-        imageUrl: flashcardProject,
-        webUrl: "https://github.com/petrihcour/flashcard-app.git",
+        imageUrl: thinkfulbnb,
+        gitHubUrl: "https://github.com/petrihcour/thinkfulbnb-project",
+        webUrl: "https://petrihcour.github.io/thinkfulbnb-project/",
     },
     {
         name: "WeLoveMovies", 
         description: "A database for a movie website, enabling users to search for their favorite movies.",
-        imageUrl: flashcardProject,
-        webUrl: "https://github.com/petrihcour/flashcard-app.git",
+        imageUrl: welovemovies,
+        gitHubUrl: "https://github.com/petrihcour/WeLoveMovies-Backend",
+        webUrl: "https://welovemovies-frontend-sjx0.onrender.com/",
     },
     {
         name: "Periodic Tables", 
         description: "A full stack application designed for fine-dining restaurant management that allows users to: create, edit, or cancel reservations; efficiently seat customers.",
-        imageUrl: flashcardProject,
-        webUrl: "https://github.com/petrihcour/flashcard-app.git",
+        imageUrl: restaurantReservation,
+        gitHubUrl: "https://github.com/petrihcour/restaurant-reservation",
+        webUrl: "https://reservation-restaurant-jkbn.onrender.com/",
     },
 ]
 
