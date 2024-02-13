b262ba775aa114e5945fe106f7dbc60abe4f431f
Vanessa <zdperfectx@gmail.com>
2024-02-13 14:34:57 -0600

    applied bootstrap flex to home vs css flex

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,24 @@
 import React from "react";
 import vanessa from "../images/vanessa.jpeg";
-import "../styles/Home.css";
-
-// add animation for the unstyled list. shift one word at a time
+import "../styles/Home.css"; 
 
 function Home() {
   return (
-    <div id="home" className="">
-      <div className="container home">
-        <div className="row align-items-center">
-          <div className="col-lg-6">
-            <h5 className="text-center">GREETINGS, TRAVELERS</h5>
-            <h1 className="text-center">I AM VANESSA</h1>
-            <p className="list-unstyled text-center">
-              human, sorcerer, software engineer, chaotic creative
-            </p>
-          </div>
-          <div className="col-lg-6 text-center">
-            <img
-              src={vanessa}
-              alt="AI representation of Vanessa"
-              className="vanessa-image border border-5 rounded-circle rounded-bottom-0"
-            />
-          </div>
+    <div id="home" className="container d-flex align-items-center justify-content-center">
+      <div className="row d-flex align-items-center">
+        <div className="col-lg-6 pt-3">
+          <h5 className="text-center">GREETINGS, TRAVELERS</h5>
+          <h1 className="text-center">I AM VANESSA</h1>
+          <p className="list-unstyled text-center">
+            a human, sorcerer, software engineer, chaotic creative
+          </p>
+        </div>
+        <div className="col-lg-6 text-center">
+          <img
+            src={vanessa}
+            alt="AI representation of Vanessa"
+            className="vanessa-image border border-5 rounded-circle rounded-bottom-0"
+          />
         </div>
       </div>
     </div>
