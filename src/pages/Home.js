945ec451f2524c4990a6eacc43b94d80d9248b96
Vanessa <zdperfectx@gmail.com>
2024-02-13 14:40:42 -0600

    adjust margin of div element home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,15 @@ import "../styles/Home.css";
 function Home() {
   return (
     <div id="home" className="container d-flex align-items-center justify-content-center">
-      <div className="row d-flex align-items-center">
-        <div className="col-lg-6 pt-3">
+      <div className="row d-flex align-items-center mt-5">
+        <div className="col-md-6 pt-3">
           <h5 className="text-center">GREETINGS, TRAVELERS</h5>
           <h1 className="text-center">I AM VANESSA</h1>
           <p className="list-unstyled text-center">
             a human, sorcerer, software engineer, chaotic creative
           </p>
         </div>
-        <div className="col-lg-6 text-center">
+        <div className="col-md-6 text-center">
           <img
             src={vanessa}
             alt="AI representation of Vanessa"
