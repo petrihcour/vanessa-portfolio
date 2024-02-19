cc5a87d688fdd29553911b8afb7eac30049d64da
Vanessa <zdperfectx@gmail.com>
2024-02-19 11:59:26 -0600

    adjust layout of features and traits

--- src/pages/FeaturesAndTraits.js
+++ src/pages/FeaturesAndTraits.js
@@ -3,31 +3,37 @@ import "../styles/FeaturesAndTraits.css";
 
 function FeaturesAndTraits() {
   return (
-    <div id="features-traits" className="bg-dark text-light">
-      <div className="mx-auto w-75">
+    <div
+      id="features-traits"
+      className="bg-dark text-light d-flex align-items-center justify-content-center"
+    >
+      <div className="w-75">
         <h2 className="text-center">FEATURES & TRAITS</h2>
-        <h5>my adventures</h5>
-      
-            <p>
-              I've found a home in the world of technology, where JavaScript,
-              React, HTML, CSS, Bootstrap, Node.js, and Express are my comfort.
-              I use these languages to create things that transcend the physical
-              world, with equal focus on beauty and functionality. In my
-              journey, there's a tale of perfect harmonies, blending my
-              background in leadership, project management, and intricate
-              problem solving with software development, resulting in a unique
-              mixture of skills and perspective.
-            </p>
-            <p>
-              Beyond coding, I'm a traveler seeking adventure far
-              and wide across the Earth. I love experiencing
-              live music, eating delicious vegan food, and (if you can't tell)
-              healthily obsessing over my favorite pastime—Dungeons &
-              Dragons.
-            </p>
-            <p>
-              If you're looking for an adventurer with an admiration for technology, an excitement for building meaningful connections and collaboration, and a history of delivering impactful results, let's go on an adventure together! You can connect with me on LinkedIn, explore my coding quests on GitHub, or simply send me a message below. We'll weave the magic of technology together and create experiences that reverberate across the internet.
-            </p>
+        <h5 className="text-center">my adventures</h5>
+        <p>
+          I've found a home in the world of technology, where JavaScript, React,
+          HTML, CSS, Bootstrap, Node.js, and Express are my comfort. I use these
+          languages to create things that transcend the physical world, with
+          equal focus on beauty and functionality. In my journey, there's a tale
+          of perfect harmonies, blending my background in leadership, project
+          management, and intricate problem solving with software development,
+          resulting in a unique mixture of skills and perspective.
+        </p>
+        <p>
+          Beyond coding, I'm a traveler seeking adventure far and wide across
+          the Earth. I love experiencing live music, eating delicious vegan
+          food, and (if you can't tell) healthily obsessing over my favorite
+          pastime—Dungeons & Dragons.
+        </p>
+        <p>
+          If you're looking for an adventurer with an admiration for technology,
+          an excitement for building meaningful connections and collaboration,
+          and a history of delivering impactful results, let's go on an
+          adventure together! You can connect with me on LinkedIn, explore my
+          coding quests on GitHub, or simply send me a message below. We'll
+          weave the magic of technology together and create experiences that
+          reverberate across the internet.
+        </p>
       </div>
     </div>
   );
