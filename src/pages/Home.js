3f0cddadccbf61291867993eb22fdf709cfd0c71
Vanessa <zdperfectx@gmail.com>
2024-03-04 12:23:42 -0600

    adjust homepage flex and spacing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,15 @@ import "../styles/Home.css";
 
 function Home() {
   return (
-    <div
-      id="home"
-      className="container"
-    >
-      <div className="row mt-5">
-        <div className="pt-3">
-          <p className="accent">
+    <div id="home" className="container">
+      <div className="d-flex align-items-start flex-column">
+          <p className="accent mb-0">
             human, sorcerer, software engineer, chaotic creative, cat mom
           </p>
-          <h1 className="title">I AM</h1>
-          <h1 className="title"> VANESSA</h1>
-        </div>
-        
+          <h1 className="home-title text-uppercase">
+            <div>I am</div>
+            <div className="home-title-vanessa d-flex align-items-start flex-column">Vanessa</div>
+          </h1>
       </div>
     </div>
   );
