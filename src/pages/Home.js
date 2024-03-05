045b294e941571f2371dfb1f7067161aa4371b15
Vanessa <zdperfectx@gmail.com>
2024-03-05 10:42:35 -0600

    adjust font color for body text and home intro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,8 +18,7 @@ function Home() {
       <div className="home-text d-flex justify-content-end text-uppercase">
         <p className="mt-3">
           <div className="text-end">I've found a home in technology,</div>
-          where JavaScript, React, HTML, CSS, Bootstrap, Node.js,
-          and Express are my comfort. I love all things beautiful and functional. I love Dungeons & Dragons, extremely spicy food, and my cats. 
+          where JavaScript, Node.js, React, HTML, CSS, and Bootstrap are my comfort. I love all things beautiful and functional. I love Dungeons & Dragons, unbearably spicy food, and my cats - Salmon, Sharky, and Fishy. 
         </p>
       </div>
     </div>
