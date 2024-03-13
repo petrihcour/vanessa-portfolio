488d57aacf5c73d3684195fe4b5cea82a4e5fc54
Vanessa <zdperfectx@gmail.com>
2024-03-13 10:33:16 -0500

    working marquee animation need to adjust

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,30 @@
 import React from "react";
 import "../styles/Home.css";
 // different animation styles for accent:
-// typewriter effect, transition is deleting the word and retyping the new word. 
-// slide effect? 
-
+// typewriter effect, transition is deleting the word and retyping the new word.
+// slide effect?
 
 function Home() {
-  const accentTexts = ["software engineer", "sorcerer", "frontend developer", "chaotic creative", "full-stack developer", "cat mom", "web developer"];
+
+  // const accentTexts = [
+  //   "software engineer",
+  //   "sorcerer",
+  //   "frontend developer",
+  //   "chaotic creative",
+  //   "full-stack developer",
+  //   "cat mom",
+  //   "web developer",
+  // ];
 
   return (
     <div id="home" className="d-flex m-sm-4">
       <div className="d-flex flex-column flex-sm-row justify-content-between">
         <div className="col-sm-6">
-          <div className="home-accent accent">
-           {accentTexts}
+          <div className="marquee home-accent accent text-lowercase">
+            <p className="text-nowrap">
+              Software Engineer, Sorcerer, Frontend Developer, Chaotic Creative,
+              Full-Stack Developer, Cat Mom, Web Developer
+            </p>
           </div>
           <h1 className="home-title text-uppercase">
             <div className="home-i-am">I am</div>
@@ -29,15 +40,14 @@ function Home() {
             Fishy.
           </p>
           <div className="d-flex justify-content-end">
-        <button
-            type="button"
-            className="btn btn-transparent border rounded-pill text-uppercase"
-          >
-            Contact Me
-          </button>
+            <button
+              type="button"
+              className="btn btn-transparent border rounded-pill text-uppercase"
+            >
+              Contact Me
+            </button>
+          </div>
         </div>
-        </div>
-        
       </div>
     </div>
   );
