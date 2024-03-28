8b82b80ec158a948b246d8444f7d860743799578
Vanessa <zdperfectx@gmail.com>
2024-03-28 10:56:21 -0500

    adjust font and margin of marquee home accent

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,7 +23,7 @@ function Home() {
 
   return (
     <div id="home" className="ms-sm-4 me-sm-4 home-container">
-      <div className="marquee-accent home-accent accent text-lowercase">
+      <div className="marquee-accent home-accent accent text-lowercase ms-3 me-3">
         <div className="marquee-accent-inner" aria-hidden="true">
           {repeatedWords.map((word, index) => (
             <div
@@ -35,39 +35,39 @@ function Home() {
           ))}
         </div>
       </div>
-      <div className="d-flex flex-column flex-lg-row justify-content-between">
-        <div className="col-lg-6">
-          <div className="home-title text-uppercase">
-            <h1 className="home-i-am display-1">I am</h1>
-            <h1 className="home-title-vanessa display-1">Vanessa</h1>
+      <div className="container-fluid">
+        <div className="row justify-content-lg-between justify-content-center align-items-end">
+          <div className="home-title text-uppercase col-lg-6">
+            <h1 className="home-i-am">I am</h1>
+            <h1 className="home-title-vanessa">Vanessa</h1>
           </div>
-        </div>
-        <div className="home-text d-flex text-uppercase row align-content-end mb-sm-4">
-          <div className="mt-2">
-            <div className="text-end">I've found a home in technology,</div>
-            where JavaScript, Node.js, React, HTML, CSS, and Bootstrap are my
-            comfort. I love all things beautiful and functional. I love Dungeons
-            & Dragons, unbearably spicy food, and my cats - Salmon, Sharky, and
-            Fishy.
-          </div>
-          <div className="d-flex justify-content-end mt-2">
-            <a
-              href="#sending"
-              type="button"
-              className="btn btn-transparent border rounded-pill text-uppercase"
-            >
-              <span className="default-text">Cast Sending</span>
-              <div className="marquee-btn" aria-hidden="true">
-                <div className="marquee-btn-inner">
-                <span>Cast Sending</span>
-                <span>Cast Sending</span>
-                <span>Cast Sending</span>
-                <span>Cast Sending</span>
-                <span>Cast Sending</span>
-                <span>Cast Sending</span>
-              </div>
-              </div>
-            </a>
+          <div className="home-text text-uppercase row align-content-center col-lg-6">
+            <div className="mt-2">
+              <div className="text-end">I've found a home in technology,</div>
+              where JavaScript, Node.js, React, HTML, CSS, and Bootstrap are my
+              comfort. I love all things beautiful and functional. I love
+              Dungeons & Dragons, unbearably spicy food, and my cats - Salmon,
+              Sharky, and Fishy.
+            </div>
+            <div className="d-flex justify-content-end mt-2">
+              <a
+                href="#sending"
+                type="button"
+                className="btn btn-transparent border rounded-pill text-uppercase"
+              >
+                <span className="default-text">Cast Sending</span>
+                <div className="marquee-btn" aria-hidden="true">
+                  <div className="marquee-btn-inner">
+                    <span>Cast Sending</span>
+                    <span>Cast Sending</span>
+                    <span>Cast Sending</span>
+                    <span>Cast Sending</span>
+                    <span>Cast Sending</span>
+                    <span>Cast Sending</span>
+                  </div>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
