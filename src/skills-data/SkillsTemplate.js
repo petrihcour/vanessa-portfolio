9fc5e28084297575ac306f136355dcc80ec0f735
Vanessa <zdperfectx@gmail.com>
2024-04-01 11:42:09 -0500

    add index to map in skills template

--- src/skills-data/SkillsTemplate.js
+++ src/skills-data/SkillsTemplate.js
@@ -6,24 +6,7 @@ function SkillsTemplate() {
     return (
         <div>
                 {skillsData.map((skill, index) => (
-                    <MarqueeButton label={skill.skill} hasMarquee />
-                    //    <button
-                    //    key={index}
-                    //    type="button"
-                    //    className="btn btn-transparent border rounded-pill text-uppercase text-light"
-                    //  >
-                    //    <span className="default-text">{skill.skill}</span>
-                    //    <div className="marquee-btn" aria-hidden="true">
-                    //      <div className="marquee-btn-inner">
-                    //        <span>{skill.skill}</span>
-                    //        <span>{skill.skill}</span>
-                    //        <span>{skill.skill}</span>
-                    //        <span>{skill.skill}</span>
-                    //        <span>{skill.skill}</span>
-                    //        <span>{skill.skill}</span>
-                    //      </div>
-                    //    </div>
-                    //  </button>
+                    <MarqueeButton index={index} label={skill.skill} hasMarquee />
                 ))}
             
         </div>
