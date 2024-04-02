81059c73e25a77d15b89db9e5fc9d8024b3c5a71
Vanessa <zdperfectx@gmail.com>
2024-04-02 10:52:22 -0500

    adjust layout of features and traits for desktop

--- src/components/FeaturesAndTraits.js
+++ src/components/FeaturesAndTraits.js
@@ -6,23 +6,23 @@ function FeaturesAndTraits() {
   return (
     <div id="features-traits" className="container">
       <div className="d-flex row md-w-75">
-        <div className="d-flex row">
-          <h2 className="features-traits-title component-title">
+        <div className="row">
+          <span className="col-lg-4">
+          <h2 className="features-traits-title component-title d-flex justify-content-end">
             FEATURES & TRAITS
           </h2>
-          <p className="accent d-flex text-lowercase">Greetings Travelers,</p>
+          <p className="accent text-lowercase d-flex justify-content-end">Greetings Travelers,</p>
+          </span>
         </div>
-        <div className="text-uppercase body-text col-md-6">
-          <p>
-            Let's go on an adventure! I create things with equal focus
-            on beauty and functionality. I blend my background in leadership,
-            project management, and intricate problem solving with software
-            development, resulting in a unique mixture of skills and
-            perspective. I've got an admiration for technology, an excitement
-            for building meaningful connections, and a history of delivering
-            impactful results. We'll weave the magic of technology and create
-            experiences that reverberate across the internet.
-          </p>
+        <div className="text-uppercase body-text col-md-7">
+          <div className="text-end">Let's go on an adventure!</div>I create
+          things with equal focus on beauty and functionality. I blend my
+          background in leadership, project management, and intricate problem
+          solving with software development, resulting in a unique mixture of
+          skills and perspective. I've got an admiration for technology, an
+          excitement for building meaningful connections, and a history of
+          delivering impactful results. We'll weave the magic of technology and
+          create experiences that reverberate across the internet.
         </div>
         <div className="col-md-6 d-flex justify-content-center">
           {/* <img
