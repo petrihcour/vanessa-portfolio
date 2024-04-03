dd9e072a14adfb5c56d925c4dece752e1bcef625
Vanessa <zdperfectx@gmail.com>
2024-04-03 11:21:38 -0500

    adjust layout for features and traits with added images

--- src/components/FeaturesAndTraits.js
+++ src/components/FeaturesAndTraits.js
@@ -1,36 +1,44 @@
 import React from "react";
-// import vanessa from "../images/self/vanessa.png";
+import vanessa from "../images/self/vanessa.jpeg";
+import vanessa2 from "../images/self/vanessa-2.JPG";
 import "../styles/FeaturesAndTraits.css";
 
 function FeaturesAndTraits() {
   return (
     <div id="features-traits" className="container">
-      <div className="d-flex row justify-content-end justify-content-md-start">
-        <div className="d-flex justify-content-end justify-content-md-start">
-          <span>
-          <h2 className="features-traits-title component-title">
-            FEATURES & TRAITS
-          </h2>
-          <p className="accent text-lowercase text-end">Greetings Travelers,</p>
-          </span>
+      <div className="row">
+        <div className="col-lg-7">
+          <div className="d-flex flex-column align-items-md-start">
+            <div className="d-flex flex-column align-items-end">
+            <h2 className="features-traits-title component-title">
+              FEATURES & TRAITS
+            </h2>
+            <p className="accent text-lowercase">Greetings Travelers,</p>
+            </div>
+          </div>
+
+          <div className="text-uppercase body-text">
+            <div className="text-end">Let's go on an adventure!</div>I create
+            things with equal focus on beauty and functionality. I blend my
+            background in leadership, project management, and intricate problem
+            solving with software development, resulting in a unique mixture of
+            skills and perspective. I've got an admiration for technology, an
+            excitement for building meaningful connections, and a history of
+            delivering impactful results. We'll weave the magic of technology
+            and create experiences that reverberate across the internet.
+          </div>
         </div>
-        <div className="text-uppercase body-text col-lg-7">
-          <div className="text-end">Let's go on an adventure!</div>I create
-          things with equal focus on beauty and functionality. I blend my
-          background in leadership, project management, and intricate problem
-          solving with software development, resulting in a unique mixture of
-          skills and perspective. I've got an admiration for technology, an
-          excitement for building meaningful connections, and a history of
-          delivering impactful results. We'll weave the magic of technology and
-          create experiences that reverberate across the internet.
-        </div>
-        <div className="col-lg-5 d-flex justify-content-lg-end align-items-center">
-        <p className="text-light">add something here. maybe an image or something animated!</p>
-          {/* <img
+        <div className="col-lg-5 d-flex flex-column justify-content-start align-items-start">
+          <img
+            src={vanessa2}
+            alt="AI representation of Vanessa"
+            className="vanessa-image img-fluid rounded mt-5 mt-md-4"
+          />
+          <img
             src={vanessa}
             alt="AI representation of Vanessa"
-            className="vanessa-image img-fluid"
-          /> */}
+            className="vanessa-image img-fluid rounded mt-3 mt-md-4"
+          />
         </div>
       </div>
     </div>
