a23aff4a357cd35f610673fc78cb8f1db0e489cd
Vanessa <zdperfectx@gmail.com>
2024-04-05 19:13:45 -0500

    add accent text to skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,6 +11,7 @@ function Skills() {
   return (
     <div id="skills" className="container">
       <h2 className="skills-title component-title">SKILLS</h2>
+      <p className="accent text-lowercase">Technologies I know and enjoy,</p>
       <SkillsTemplate />
     </div>
   );
