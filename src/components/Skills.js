591b2678055204f34704f24d3c6299d016888b0e
Vanessa <zdperfectx@gmail.com>
2024-04-05 19:28:26 -0500

    adjust skills component title and accent layout

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,16 +2,19 @@ import React from "react";
 import SkillsTemplate from "../skills-data/SkillsTemplate";
 import "../styles/Skills.css";
 
-
 // add dice rolling for bonuses for fun after hitting button
 
 function Skills() {
- 
-
   return (
     <div id="skills" className="container">
-      <h2 className="skills-title component-title">SKILLS</h2>
-      <p className="accent text-lowercase">Technologies I know and enjoy,</p>
+      <div className="row">
+        <div className="col">
+          <div className="d-flex flex-column align-items-end align-items-md-start">
+            <h2 className="skills-title component-title">SKILLS</h2>
+            <p className="accent text-lowercase">Things I'm good at,</p>
+          </div>
+        </div>
+      </div>
       <SkillsTemplate />
     </div>
   );
