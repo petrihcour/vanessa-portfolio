f45f43b007e47ef1db97ab110647d162bb8990f6
Vanessa <zdperfectx@gmail.com>
2024-04-09 10:32:55 -0500

    add section class for layout all components

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -11,21 +11,33 @@ function Layout() {
   return (
     <div className="Layout">
       <Header />
-        <div className="bg-home min-vh-100 container-fluid" id="home">
-          <Home  />
-        </div>
-        <div className="bg-featuresAndTraits min-vh-100 container-fluid" id="features-traits">
-          <FeaturesAndTraits />
-        </div>
-        <div className="bg-skills min-vh-100 container-fluid" id="skills">
-          <Skills />
-        </div>
-        <div className="bg-quests min-vh-100 container-fluid" id="quests">
-          <Quests />
-        </div>
-        <div className="bg-sending min-vh-100 container-fluid" id="sending">
-          <Sending />
-        </div>
+      <div className="section bg-home min-vh-100 container-fluid" 
+      id="home"
+      >
+        <Home />
+      </div>
+      <div
+        className="section bg-featuresAndTraits min-vh-100 container-fluid"
+        id="features-traits"
+      >
+        <FeaturesAndTraits />
+      </div>
+      <div className="section bg-skills min-vh-100 container-fluid" 
+      id="skills"
+      >
+        <Skills />
+      </div>
+      <div className="section bg-quests min-vh-100 container-fluid" 
+      id="quests"
+      >
+        <Quests />
+      </div>
+      <div
+        className="section bg-sending min-vh-100 container-fluid"
+        id="sending"
+      >
+        <Sending />
+      </div>
     </div>
   );
 }
