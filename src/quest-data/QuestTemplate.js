737caffc7bc9fc3ae82fb6d23906678a70058e27
Vanessa <zdperfectx@gmail.com>
2024-04-15 11:26:28 -0500

    starting quest layout UI from scratch

--- src/quest-data/QuestTemplate.js
+++ src/quest-data/QuestTemplate.js
@@ -1,35 +1,18 @@
 import React, { useState } from "react";
-import { Button, Card } from "react-bootstrap";
 import QuestData from "./QuestData";
 import "../styles/Quests.css";
 
 
 function QuestTemplate() {
-  const [expandedIndex, setExpandedIndex] = useState(null);
-
-  const handleCardClick = (index) => {
-    setExpandedIndex((prevIndex) => (prevIndex === index ? null : index));
-  };
 
   return (
-    <div className="m-3">
-      {QuestData.map((quest, index) => (
-        <div
-          key={index}
-          className={`quest-card ${expandedIndex === index ? "expanded" : ""} pe-3`}
-          onClick={() => handleCardClick(index)}
-        >
-          <Card style={{ height: "" /* Set a fixed height for each card */ }}>
-            <Card.Img variant="top" src={quest.imageUrl} />
-            <Card.Body>
-              <Card.Title>{quest.name}</Card.Title>
-              <Card.Text>{quest.description}</Card.Text>
-              <Button href={quest.webUrl} target="_blank" className="btn-sm m-1">Live Demo</Button>
-              <Button href={quest.gitHubUrl} target="_blank" className="btn-sm m-1">Github</Button>
-            </Card.Body>
-          </Card>
-        </div>
-      ))}
+    <div>
+
+          
+              <button  className="btn btn-sm">Live Demo</button>
+              <button  className="btn btn-sm">Github</button>
+          
+ 
     </div>
   );
 }
