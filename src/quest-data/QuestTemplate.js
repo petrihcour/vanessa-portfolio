3e8e05b4e17d9e8f4c0737d69e0aa36bf57a21ae
Vanessa <zdperfectx@gmail.com>
2024-04-15 11:46:22 -0500

    baseline of quest layout set with font and image color

--- src/quest-data/QuestTemplate.js
+++ src/quest-data/QuestTemplate.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import QuestData from "./QuestData";
 import "../styles/Quests.css";
 
-
 function QuestTemplate() {
+  const quests = QuestData.map((quest, index) => (
+    <div key={index}>
+      <h4 className="component-title-sm text-uppercase">{quest.name}</h4>
+      <p className="text-uppercase body-text fs-6">{quest.description}</p>
+      <button href={quest.webUrl} className="btn btn-sm">Live Demo</button>
+      <button href={quest.gitHubUrl} className="btn btn-sm">Github</button>
+      <img src={quest.imageUrl} className="w-50"  alt={`${quest.name} application project screenshot`} />
+    </div>
+  ));
 
   return (
     <div>
-
-          
-              <button  className="btn btn-sm">Live Demo</button>
-              <button  className="btn btn-sm">Github</button>
-          
- 
+      {quests}
     </div>
   );
 }
