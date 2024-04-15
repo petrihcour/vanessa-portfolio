cf2e9076f0353cc03e41fe36dca9b205e815975d
Vanessa <zdperfectx@gmail.com>
2024-04-15 11:54:27 -0500

    add button marquee to all buttons

--- src/quest-data/QuestTemplate.js
+++ src/quest-data/QuestTemplate.js
@@ -1,14 +1,18 @@
 import React from "react";
 import QuestData from "./QuestData";
+import MarqueeButton from "../animations/MarqueeButton";
 import "../styles/Quests.css";
+
+// need to adjust button to be small for quests
 
 function QuestTemplate() {
   const quests = QuestData.map((quest, index) => (
     <div key={index}>
       <h4 className="component-title-sm text-uppercase">{quest.name}</h4>
       <p className="text-uppercase body-text fs-6">{quest.description}</p>
-      <button href={quest.webUrl} className="btn btn-sm">Live Demo</button>
-      <button href={quest.gitHubUrl} className="btn btn-sm">Github</button>
+      <MarqueeButton href={quest.webUrl} label="Live Demo" hasMarquee className="btn-sm" />
+      <MarqueeButton href={quest.gitHubUrl} label="GitHub" hasMarquee className="btn-sm" />
+      <br />
       <img src={quest.imageUrl} className="w-50"  alt={`${quest.name} application project screenshot`} />
     </div>
   ));
