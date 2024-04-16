abb520a8f01835b9f1f4f52cd327fcea4879e9f0
Vanessa <zdperfectx@gmail.com>
2024-04-16 12:37:34 -0500

    add accent image

--- src/components/Quests.js
+++ src/components/Quests.js
@@ -1,5 +1,6 @@
 import React from "react";
 import QuestTemplate from "../quest-data/QuestTemplate";
+import star from "../images/accents/star.png";
 
 // need to fix how quests page looks and determine how i want to display my projects. it does not look good now
 // do i want cards with each project? do i want a slide? make it smaller?
@@ -10,10 +11,11 @@ function Quests() {
       <div className="row">
         <div className="col">
           <div className="d-flex flex-column align-items-end align-items-md-start">
-            <p className="accent text-lowercase">Projects I have created,</p>
+            <p className="accent text-lowercase"> <img src={star} alt="13 pointed star" className="star-img me-3" /> Projects I have created,</p>
           </div>
         </div>
       </div>
+      
       <div className="body-text text-uppercase">
         <div className="text-end">Quests are how I level up!</div>
         They're a big part of my adventures, showcasing tangible examples of my
