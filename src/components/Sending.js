868b128f49c9397899965763a5397d64f901c79e
Vanessa <zdperfectx@gmail.com>
2024-04-24 10:07:26 -0500

    match layout of sending component to home component

--- src/components/Sending.js
+++ src/components/Sending.js
@@ -47,87 +47,96 @@ export const Sending = () => {
   };
 
   return (
-    <div id="sending" className="container-fluid">
-      <h2 className="component-title home-title text-uppercase">Sending</h2>
-      <form
-        ref={formRef}
-        className="card"
-        onSubmit={handleSubmit}
-        style={{ backgroundColor: "#15171C" }}
-      >
-        <div className="card-body">
-          <div className="mb-3">
-            <label
-              htmlFor="formGroupExampleInput"
-              className="form-label body-text text-uppercase"
-            >
-              Your Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              placeholder="What's your name?"
-              className="form-control body-text text-uppercase"
-              style={{ color: "#404455" }}
-              id="name"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label
-              htmlFor="exampleFormControlInput1"
-              className="form-label body-text text-uppercase"
-            >
-              your email
-            </label>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              placeholder="email@myemail.com"
-              className="form-control body-text text-uppercase"
-              style={{ color: "#404455" }}
-              id="email"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label
-              htmlFor="exampleFormControlTextarea1"
-              className="form-label body-text text-uppercase"
-            >
-              your message
-            </label>
-            <textarea
-              rows="3"
-              name="message"
-              value={form.message}
-              placeholder="I want to hire you!"
-              className="form-control body-text text-uppercase"
-              style={{ color: "#404455" }}
-              id="message"
-              onChange={handleChange}
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="btn btn-outline"
-            style={{ border: "none", padding: "0" }}
-          >
-            <MarqueeButton
-              label={loading ? "sending" : "send"}
-              hasMarquee
-              style={{
-                backgroundColor: "#F2F2F5",
-                color: "#15171C",
-                fontWeight: "600",
-              }}
-              textColor="#15171C"
-            />
-          </button>
+    <div id="sending" className="container-fluid mt-md-5 pt-md-5">
+      <div className="justify-content-xl-between row">
+        <div className="text-uppercase col-lg-6 align-content-end">
+          <h1 className="home-i-am">Let's</h1>
+          <h1 className="home-title-vanessa">Connect</h1>
         </div>
-      </form>
+        <div className="col-xl-5 col-lg-6 mt-4 align-content-end">
+          <form
+            ref={formRef}
+            className="card"
+            onSubmit={handleSubmit}
+            style={{ backgroundColor: "#15171C" }}
+          >
+            <div className="card-body">
+              <div className="mb-3">
+                <label
+                  htmlFor="formGroupExampleInput"
+                  className="form-label body-text text-uppercase"
+                >
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  placeholder="What's your name?"
+                  className="form-control body-text text-uppercase"
+                  style={{ color: "#404455" }}
+                  id="name"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label body-text text-uppercase"
+                >
+                  your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={form.email}
+                  placeholder="email@myemail.com"
+                  className="form-control body-text text-uppercase"
+                  style={{ color: "#404455" }}
+                  id="email"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlTextarea1"
+                  className="form-label body-text text-uppercase"
+                >
+                  your message
+                </label>
+                <textarea
+                  rows="3"
+                  name="message"
+                  value={form.message}
+                  placeholder="I want to hire you!"
+                  className="form-control body-text text-uppercase"
+                  style={{ color: "#404455" }}
+                  id="message"
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+              <div className="d-flex justify-content-end mt-4">
+                <button
+                  type="submit"
+                  className="btn btn-outline"
+                  style={{ border: "none", padding: "0" }}
+                >
+                  <MarqueeButton
+                    label={loading ? "sending" : "send"}
+                    hasMarquee
+                    style={{
+                      backgroundColor: "#F2F2F5",
+                      color: "#15171C",
+                      fontWeight: "600",
+                    }}
+                    textColor="#15171C"
+                  />
+                </button>{" "}
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
