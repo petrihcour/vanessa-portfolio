d99294f1afa4e0cbfe4a4eabffe2c28b3f1318e0
Vanessa <zdperfectx@gmail.com>
2024-05-07 10:20:19 -0500

    adjust animation to trigger when element becomes visible

--- src/animations/ScrollAnimation.js
+++ src/animations/ScrollAnimation.js
@@ -10,8 +10,8 @@ const ScrollAnimation = ({ children, animation }) => {
       const rect = element.getBoundingClientRect();
       const windowHeight = window.innerHeight;
 
-      // Trigger animation when element is 2% visible
-      if (rect.top < windowHeight * 0.75 && rect.bottom >= 0) {
+      // Trigger animation when element is visible
+      if (rect.top < windowHeight * 1 && rect.bottom >= 0) {
         setIsVisible(true);
       } else {
         setIsVisible(false);
