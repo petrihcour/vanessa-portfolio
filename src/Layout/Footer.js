71e20b77de9e2ce6c62611b2ba93f1cbf5ccd519
Vanessa <zdperfectx@gmail.com>
2024-05-14 10:19:42 -0500

    add animation to footer

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,41 +1,49 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { staggerContainer, staggerItem } from "../animations/animations";
+import { useInView } from "react-intersection-observer";
 import "../styles/Footer.css";
 
 function Footer() {
+  const { ref, inView } = useInView({ triggerOnce: false });
+
+  const links = [
+    {
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/in/vanesssagarcia/",
+    },
+    {
+      label: "GitHub",
+      url: "https://github.com/petrihcour",
+    },
+    {
+      label: "Resume",
+      url: "to be filled out",
+    },
+  ];
+
   return (
     <nav className="sending-footer body-text d-flex text-uppercase justify-content-center justify-content-xl-end mt-2">
-      <ul className="nav">
-        <li className="nav-item">
-          <a
-            className="nav-link"
-            href="https://www.linkedin.com/in/vanesssagarcia/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link"
-            href="https://github.com/petrihcour"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link"
-            href="to be filled out"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Resume
-          </a>
-        </li>
-      </ul>
+      <motion.ul
+        className="nav"
+        ref={ref}
+        variants={staggerContainer}
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"}
+      >
+        {links.map((link, index) => (
+          <motion.li key={index} variants={staggerItem}>
+            <a
+              className="nav-link"
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.label}
+            </a>
+          </motion.li>
+        ))}
+      </motion.ul>
     </nav>
   );
 }
